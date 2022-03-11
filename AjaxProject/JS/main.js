c39fe9941b4e0968037b5aba39d746de46a2fa38
tead1234 <tead1234@gmail.com>
2022-03-11 09:53:58 +0900

    foreach

--- AjaxProject/JS/main.js
+++ AjaxProject/JS/main.js
@@ -2,15 +2,18 @@
 var options = `<option>95</option>
 <option>100</option>
 <option>105</option>`
-var options_pants = `<option>80</option>
-<option>82</option>
-<option>84</option>`
+var size_pants = [80,82,84,86,88];
+//array foreach array[i] 이게 아니라 array.foreach(fun(i){}); 콜백 반복문으로 바꿈
 $('#option1').on('change', function(){
     if ($('#option1').val() == "셔츠"){
         $("#option2").html("");
         $("#option2").append(options);
-    }else{
+    }else if($('#option1').val() == "바지"){
         $("#option2").html("");
-        $("#option2").append(options_pants);
-    }
+        size_pants.forEach(function(i){
+            var options_pants = `<option>${i}</option>`
+            $("#option2").append(options_pants);
+        })
+            
+}
 })
