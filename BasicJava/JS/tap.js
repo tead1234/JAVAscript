8b959bf0ae3273432887a2aa40920d12ceb1d517
tead1234 <tead1234@gmail.com>
2022-03-16 23:47:13 +0900

    test

--- BasicJava/JS/tap.js
+++ BasicJava/JS/tap.js
@@ -45,13 +45,46 @@ function getLocation() {
 getLocation();
 var xhr = new XMLHttpRequest();
 var url = 'http://api.data.go.kr/openapi/tn_pubr_public_traffic_light_api'; /*URL*/
-var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'hFv6v8mxbGG4c5TIwfG/wYNVgHDJz61zqizx9iuW2VJeqBQqHbg28JUK+XMpb69vPpU6X+xIrbQMPk7eGUqp8w=='; /*Service Key*/
+var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'hFv6v8mxbGG4c5TIwfG%2FwYNVgHDJz61zqizx9iuW2VJeqBQqHbg28JUK%2BXMpb69vPpU6X%2BxIrbQMPk7eGUqp8w%3D%3D'; /*Service Key*/
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('0'); /**/
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
+queryParams += '&' + encodeURIComponent('type') + '=' + encodeURIComponent('xml'); /**/
+queryParams += '&' + encodeURIComponent('ctprvnNm') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('signguNm') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('roadKnd') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('roadRouteNo') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('roadRouteNm') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('roadRouteDrc') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('rdnmadr') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('lnmadr') + '=' + encodeURIComponent(''); /**/
 queryParams += '&' + encodeURIComponent('latitude') + '=' + encodeURIComponent(''); /**/
 queryParams += '&' + encodeURIComponent('longitude') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('sgngnrInstlMthd') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('roadType') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('priorRoadYn') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('tfclghtManageNo') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('tfclghtSe') + '=' + encodeURIComponent(''); /**/
 queryParams += '&' + encodeURIComponent('tfclghtColorKnd') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('sgnaspMthd') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('sgnaspOrdr') + '=' + encodeURIComponent(''); /**/
 queryParams += '&' + encodeURIComponent('sgnaspTime') + '=' + encodeURIComponent(''); /**/
-
+queryParams += '&' + encodeURIComponent('sotKnd') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('signlCtrlMthd') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('signlTimeMthdType') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('opratnYn') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('flashingLightOpenHhmm') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('flashingLightCloseHhmm') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('fnctngSgngnrYn') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('remndrIdctYn') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('sondSgngnrYn') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('drcbrdSn') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('institutionNm') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('phoneNumber') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('referenceDate') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('instt_code') + '=' + encodeURIComponent(''); /**/
+queryParams += '&' + encodeURIComponent('instt_nm') + '=' + encodeURIComponent(''); /**/
 xhr.open('GET', url + queryParams);
+
 xhr.onreadystatechange = function () {
     if (this.readyState == 4) {
         alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
