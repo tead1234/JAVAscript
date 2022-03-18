8cd584dfb9fa93aa124847389e05a8d018b25e06
tead1234 <tead1234@gmail.com>
2022-03-18 17:59:13 +0900

    카트메누 만들기

--- AjaxProject/JS/main.js
+++ AjaxProject/JS/main.js
@@ -1,19 +1,22 @@
 
-var options = `<option>95</option>
-<option>100</option>
-<option>105</option>`
+const options = [90,95,100,105];
 var size_pants = [80,82,84,86,88];
 //array foreach array[i] 이게 아니라 array.foreach(fun(i){}); 콜백 반복문으로 바꿈
+// arrow를 쓰면 this값이 달라짐 ex function으로 함수속에서 쓰면 this가 재정의 arrow는 재정의 x
 $('#option1').on('change', function(){
     if ($('#option1').val() == "셔츠"){
         $("#option2").html("");
-        $("#option2").append(options);
-    }else if($('#option1').val() == "바지"){
-        $("#option2").html("");
-        size_pants.forEach(function(i){
-            var options_pants = `<option>${i}</option>`
-            $("#option2").append(options_pants);
+        options.forEach(function(e){
+            let shirt_option = `<option>${e}</option>`
+            $("#option2").append(shirt_option);
         })
+    }
+    else if($('#option1').val() == "바지"){
+        $("#option2").html("");
+        size_pants.forEach(function(a){
+            var options_pants = `<option>${a}</option>`
+            $("#option2").append(options_pants);
+        });
             
 }
 })
