14c938d91d6b8a601e8cdf4c308cf0f860e34990
tead1234 <tead1234@gmail.com>
2022-06-27 05:03:01 +0900

    자바 코테 준비

--- ZeroBase_FrontEnd/main.js
+++ ZeroBase_FrontEnd/main.js
@@ -3,6 +3,14 @@ let arr = [
     [20,21,22],
     [30,31,32]
 ]
+let input = [
+    4,100, 500,247
+]
+let input_date=[
+    "2021-01-27",
+    "2021-02-27",
+    "2021-03-14"
+]
 
 
 let star2 = function star(num) {
@@ -25,4 +33,20 @@ let between = (a,b)=> {
     return answer
 }
 
-console.log(between(15,10))
+const find_moon_year = (year) => {
+        if(year % 400 == 0){
+            return true;
+        }else if(year % 4 == 0 && year % 100 != 0 ){
+            return true;
+        }
+        return false
+}
+const find_date = (date)=>{
+    let day = new Date(date);
+    const weekday = ['일','월','화','수','목','금','토'];
+    return weekday[day.getDay()]
+}
+
+for(let i =0; i < input_date.length; i ++){
+    console.log(`#${i+1} ` + find_date(input_date[i]));
+}
